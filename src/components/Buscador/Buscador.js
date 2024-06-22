a799cf673064bf46430bc2f175c17453818f9ee6
Jose Rodriguez <128047100+JoseRodriguezCa@users.noreply.github.com>
2024-06-22 12:16:14 +0200

    Update Buscador.js

--- src/components/Buscador/Buscador.js
+++ src/components/Buscador/Buscador.js
@@ -10,9 +10,9 @@ export const buscador = () => {
   const divBuscador = document.createElement("div");
   const buscadorInput = document.createElement("input");
   const pruebaInput = document.createElement("input");
-  const iBuscador = document.createElement("i");
+  // const iBuscador = document.createElement("i");
 
-  iBuscador.classList = "fa-solid fa-magnifying-glass";
+  // iBuscador.classList = "fa-solid fa-magnifying-glass";
   buscadorInput.type = "text";
   pruebaInput.type = "text"
   buscadorInput.placeholder = "Buscar Eventos";
@@ -28,7 +28,7 @@ export const buscador = () => {
     divBuscador.append(btnBuscar)
   }
 
-  divBuscador.append(iBuscador, pruebaInput);
+  divBuscador.append( pruebaInput);
   return divBuscador;
 };
 
