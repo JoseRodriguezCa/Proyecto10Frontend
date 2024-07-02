f4854be5ffe332c8b11cca0868c2cb5bd9e5b564
Jose Rodriguez <128047100+JoseRodriguezCa@users.noreply.github.com>
2024-07-02 13:22:08 +0200

    a
    a

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -132,14 +132,14 @@ export const mobileHeader = () => {
         </svg>
       </button>
       <a href="/login" class="tab login">
-        <svg class="svgIcon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <svg class="svgIcon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
           <path d="M14 8v-2a2 2 0 0 0 -2 -2h-5a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h5a2 2 0 0 0 2 -2v-2" />
           <path d="M20 12h-13l3 -3m0 6l-3 -3" />
         </svg>
       </a>
       <a href="/register" class="tab register">
-        <svg class="svgIcon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <svg class="svgIcon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
           <circle cx="12" cy="8" r="4" />
           <path d="M6 20v-1a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v1" />
